7836e515b66039b1dbf3f8eb68a216b98ee4d4d3
Deepak Chawla <deepakchawla078@gmail.com>
2022-01-18 14:41:38 +0500

    added updateOrderToPaid Route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderControllers.js'
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderControllers.js'
 import { protect } from '../middlewares/authMiddlewares.js'
 
 router.route('/order').post(protect, addOrderItems)
 router.route('/order/:id').get(protect, getOrderById)
+router.route('/order/:id/pay').put(protect, updateOrderToPaid)
 
 export default router
