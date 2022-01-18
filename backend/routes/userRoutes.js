2affa05cfa68977e366361415279798e93de2ef9
Deepak Chawla <deepakchawla078@gmail.com>
2022-01-18 20:39:22 +0500

    added user routes for admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,16 +2,22 @@ import express from 'express'
 const router = express.Router()
 import {
   deleteUser,
+  getUserById,
   getUserProfile,
   getUsers,
   login,
   register,
+  updateUser,
   updateUserProfile,
 } from '../controllers/userControllers.js'
 import { admin, protect } from '../middlewares/authMiddlewares.js'
 
 router.route('/').get(protect, admin, getUsers)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .put(protect, admin, updateUser)
+  .get(protect, admin, getUserById)
 router.post('/register', register)
 router.post('/login', login)
 router
