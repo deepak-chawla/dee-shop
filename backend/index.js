0b850dcd4c8e90f919476ce5c8fb4525422660a4
Deepak Chawla <deepakchawla078@gmail.com>
2022-04-11 01:15:32 +0500

    prepare for production

--- backend/index.js
+++ backend/index.js
@@ -12,24 +12,38 @@ import uploadRouter from './routes/uploadRoutes.js'
 
 const app = express()
 dotenv.config()
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
+
 connectDB()
 
 app.use(cors())
 app.use(express.json())
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use('/api/upload', uploadRouter)
 app.use('/api', orderRouter)
 app.use('/api', productRouter)
 app.use('/api/user', userRouter)
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')))
+  app.get('*', (res, req) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(2000, () => {
-  console.log('Server is running on port 2000')
+const PORT = process.env.PORT || 2000
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on ${process.env.NODE_ENV} mode in port ${PORT}`
+  )
 })
